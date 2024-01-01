a2eac7aadb2bcf7be8551c3fb6c067bcaf78c0d5
Aria <47712536+MrAriaNet@users.noreply.github.com>
2024-01-02 00:47:12 +0330

    Update get.sh

--- get.sh
+++ get.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-echo "/ip firewall address-list remove [/ip firewall address-list find list=NoNAT]";
-echo "/ip firewall address-list";
+do_update_ipir() {
+    echo "/ip firewall address-list remove [/ip firewall address-list find list=NoNAT]";
+    echo "/ip firewall address-list";
+    
+    for range in $( curl --silent -X POST -H 'Connection: close' "https://stat.ripe.net/data/country-resource-list/data.json?resource=IR&v4_format=prefix" | jq .data.resources.ipv4[] | sed 's/"//g' ); do
+        echo ":do { add address=$range list=NoNAT} on-error={}";
+    done;
+    
+    echo ":do { add address=10.0.0.0/8 list=NoNAT} on-error={}";
+}
 
-for range in $( curl --silent -X POST -H 'Connection: close' "https://stat.ripe.net/data/country-resource-list/data.json?resource=IR&v4_format=prefix" | jq .data.resources.ipv4[] | sed 's/"//g' ); do
-    echo ":do { add address=$range list=NoNAT} on-error={}";
-done;
-
-echo ":do { add address=10.0.0.0/8 list=NoNAT} on-error={}";
-
-> list.rsc
+do_update_ipir > list.rsc
